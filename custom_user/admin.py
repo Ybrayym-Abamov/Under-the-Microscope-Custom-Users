626c27c8a1ec0a1376bcc1c72b09beb39ba6b4b4
Ybrayym <ybrayymjan90@gmail.com>
2020-05-22 23:55:24 -0400

    fixed a url bug that was preventing redirection to the homepage

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from custom_user.models import MyUser
 
+
 admin.site.register(MyUser, UserAdmin)
--- custom_user/models.py
+++ custom_user/models.py
@@ -1,5 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class MyUserManager(BaseUserManager):
+    def create_super_user(self, email, age, password=None):
+        """
+        Creates and saves a superuser with the given email, age, and password.
+        """
+        user = self.create_user(
+            email=email,
+            password=password,
+            age=age
+        )
+        user.as_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class MyUser(AbstractUser):
